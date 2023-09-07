4d9ab2fa2b03604a98c2637b3c16d0abc064b350
Kwanhooo <kwanhooo@foxmail.com>
2023-09-07 20:50:37 +0800

    feat: 模板库

--- sql/template_lib_09_07.sql
+++ sql/template_lib_09_07.sql
@@ -1,74 +1,98 @@
 -- 插入25K车型的所有零部件类型
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (123456789, '25K_all_wheel', '25K', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (987654321, '25K_l_spring', '25K', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (567890123, '25K_s_spring', '25K', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (345678901, '25K_bearing', '25K', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (789012345, '25K_wheel', '25K', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (234567890, '25K_clamp', '25K', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (456789012, '25K_b_box', '25K', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (890123456, '25K_s_box', '25K', null, null, null, null, null, null, 0);
 
 -- 插入25G车型的所有零部件类型
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (112233445, '25G_all_wheel', '25G', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (223344556, '25G_l_spring', '25G', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (334455667, '25G_s_spring', '25G', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (445566778, '25G_bearing', '25G', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (556677889, '25G_wheel', '25G', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (667788990, '25G_clamp', '25G', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (778899001, '25G_b_box', '25G', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (889900112, '25G_s_box', '25G', null, null, null, null, null, null, 0);
 
 -- 插入25T车型的所有零部件类型
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (990011223, '25T_all_wheel', '25T', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (100112334, '25T_l_spring', '25T', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (200223445, '25T_s_spring', '25T', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (300334556, '25T_bearing', '25T', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (400445667, '25T_wheel', '25T', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (500556778, '25T_clamp', '25T', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (600667889, '25T_b_box', '25T', null, null, null, null, null, null, 0);
 
-INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime, isDeleted)
+INSERT INTO tvds.templates_lib (dbId, id, model, cameraNumber, templateUrl, createYear, version, createTime, updateTime,
+                                isDeleted)
 VALUES (700778900, '25T_s_box', '25T', null, null, null, null, null, null, 0);
